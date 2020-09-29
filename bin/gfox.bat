524c915b1bb491e65387892fec3d9a13a59f157c
Hans-Juergen Rennau <hrennau@yahoo.de>
2020-09-29 20:59:05 +0200

    Adapted .bat to new report names.

--- bin/gfox.bat
+++ bin/gfox.bat
@@ -12,8 +12,7 @@ set HERE=%XERE:\=/%
 :: ====================================================================================
 
 :: defaults
-set RTYPE=
-set ISWHITE=
+set RTYPE=sum2
 
 :NEXTPAR
 set name=%~1
@@ -28,15 +27,12 @@ set value=%~1
 
 if "%value%"=="" goto :ENDPAR
 
-if "%name%"=="-w" (
-   set ISWHITE=Y
-) else if "%name%"=="-r" (
-   set ISWHITE=N
-) else if "%name%"=="-t" (
-   set RTYPE=!VALUE!
-   shift   
- ) else if "%name%"=="-t" (
-   set RTYPE=!VALUE!
+if "%name%"=="-a" (set RTYPE=sum1
+) else if "%name%"=="-b" (set RTYPE=sum2
+) else if "%name%"=="-c" (set RTYPE=sum3
+) else if "%name%"=="-r" (set RTYPE=red
+) else if "%name%"=="-w" (set RTYPE=white
+) else if "%name%"=="-t" (set RTYPE=!VALUE!
    shift   
  ) else (
    echo Unknown option: %name%
@@ -48,31 +44,34 @@ if "%name%"=="-w" (
 goto :NEXTPAR
 :ENDPAR 
 
-if "%RTYPE"=="" (
-    if "%ISWHITE%"=="Y" (
-        set RTYPE=whiteTree
-    ) else if "%RTYPE%"=="w" (
-        set RTYPE=whiteTree
-    ) else if "%RTYPE%"=="r" (
-        set RTYPE=redTree
-    ) else (
-        echo "Unknown report type: %RTYPE%
-        echo Aborted.
-        exit /b
-    )
+if "%RTYPE%"=="white" (rem        
+) else if "%RTYPE%"=="red" (rem
+) else if "%RTYPE%"=="sum1" (rem
+) else if "%RTYPE%"=="sum2" (rem 
+) else if "%RTYPE%"=="sum3" (rem        
+) else (
+     echo "Unknown report type: %RTYPE%
+     echo Aborted.
+     exit /b
 )
+
 set schema=%name%
 shift
 set domain=%1
 
 if "%schema%"=="?" (
     echo.
-    echo Usage: greenfox [-f w] schema [domain]
+    echo Usage: greenfox [-a -b -c -r -w] [-t type] schema [domain]
     echo.
     echo schema: Greenfox schema file; relative or absolute path
     echo domain: Validation root resource; relative or absolute path
     echo.
-    echo -t      : the report type; w=whiteTree; r=redTree 
+    echo -a      : report type = sum1
+    echo -b      : report type = sum2
+    echo -c      : report type = sum3        
+    echo -r      : report type = red
+    echo -w      : report type = white        
+    echo -t      : the report type; one of: sum1 sum2 sum3 red white wresults rresults 
     echo.
     exit /b
 )
