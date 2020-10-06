17f320a72c6558bddcbaa18f8f00bf61e551f98b
Hauke Brandes <hauke.brandes@parsqube.de>
2020-10-06 13:58:15 +0200

    Clean up inconsistencies in Windows CLI

--- bin/gfox.bat
+++ bin/gfox.bat
@@ -33,31 +33,32 @@ if "%name%"=="" goto :ENDPAR
 shift
 set value=%~1
 
-if "%name%"=="-1" (set RTYPE=sum1
+if        "%name%"=="-1" (set RTYPE=sum1
 ) else if "%name%"=="-2" (set RTYPE=sum2
 ) else if "%name%"=="-3" (set RTYPE=sum3
 ) else if "%name%"=="-r" (set RTYPE=red
 ) else if "%name%"=="-w" (set RTYPE=white
 ) else if "%name%"=="-t" (set RTYPE=!VALUE!
-   shift   
+   shift
 ) else if "%name%"=="-p" (
    if "%PARAMS%"=="" (set PARAMS=!VALUE!) else (
        set EDITED=!VALUE!
        rem set PARAMS=!PARAMS!;!VALUE!
        set PARAMS=!PARAMS!;!EDITED!
    )
-   shift   
+   shift
 ) else if "%name%"=="-C" (
    set CCFILTER=!VALUE!
-   shift   
+   shift
 ) else if "%name%"=="-R" (
    set FNFILTER=!VALUE!
-   shift   
+   shift
  ) else if "%char1%"=="-" (
    echo Unknown option: %name%
-   echo Supported options: 
+   echo Supported options:
    echo    -1 -2 -3 -r -w                 # Select report type
-   echo    -p "name=value"                # Set schema parameter - multiple use allowed
+   echo    -t reportType                  # Set report type to specific type
+   echo    -p "name:value"                # Set schema parameter - multiple use allowed
    echo    -C "foo* *bar ~foo*bar ~*peng" # Filter constraint components - all "foo* or *bar, except foo*bar or *peng"   
    echo    -R "airport-* ~*.fra.*"        # Filter resources - all "airport-*, except *.fra.*"   
    echo Aborted.
@@ -70,7 +71,7 @@ if "%name%"=="-1" (set RTYPE=sum1
     ) else (
         echo Usage: gfox schema [domain] [-1 -2 -3 -r -w] [-t type] [-p params] [-C constraintTypes] [-R resourceNames]
         echo.
-        echo Third parameter not allowed.
+        echo Third argument not allowed.
         exit /b    
     )
 )
@@ -78,17 +79,17 @@ if "%name%"=="-1" (set RTYPE=sum1
 if "%value%"=="" goto :ENDPAR
 
 goto :NEXTPAR
-:ENDPAR 
+:ENDPAR
 
 rem echo schema=!schema!
 rem echo domain=!domain!
 rem echo rtype=!rtype!
 
-if "%RTYPE%"=="white" (rem        
-) else if "%RTYPE%"=="red" (rem
-) else if "%RTYPE%"=="sum1" (rem
-) else if "%RTYPE%"=="sum2" (rem 
-) else if "%RTYPE%"=="sum3" (rem        
+if        "%RTYPE%"=="white"    (rem
+) else if "%RTYPE%"=="red"      (rem
+) else if "%RTYPE%"=="sum1"     (rem
+) else if "%RTYPE%"=="sum2"     (rem
+) else if "%RTYPE%"=="sum3"     (rem
 ) else if "%RTYPE%"=="wresults" (rem
 ) else if "%RTYPE%"=="rresults" (rem
 ) else (
@@ -103,29 +104,29 @@ rem set domain=%1
 
 if "%schema%"=="?" (
     echo.
-    echo Usage: gfox schema [domain] [-1 -2 -3 -r -w] [-t type] [-p params] [-C constraintTypes] [-R resourceNames] 
+    echo Usage: gfox schema [domain] [-1 -2 -3 -r -w] [-t type] [-p paramBinding] [-C constraintTypes] [-R resourceNames]
     echo.
-    echo Options and parameters may be mixed.
+    echo Options and arguments may be mixed.
     echo.
     echo schema: Greenfox schema file; relative or absolute path
     echo domain: Validation root resource; relative or absolute path
     echo.
     echo -1      : report type = sum1
     echo -2      : report type = sum2
-    echo -3      : report type = sum3        
+    echo -3      : report type = sum3
     echo -r      : report type = red
-    echo -w      : report type = white        
-    echo -t      : the report type; one of: sum1 sum2 sum3 red white wresults rresults
-    echo -p foo:bar          : parameter with name foo and value bar 
+    echo -w      : report type = white
+    echo -t reportType       : the report type; one of: sum1 sum2 sum3 red white wresults rresults
+    echo -p foo:bar          : parameter with name foo and value bar
     echo -C constraintTypes  : filter by constraint type
     echo -R resourceNames    : filter by resource name
-    echo.       
-    echo The values of -F and -R are interpreted as "name filter"; rules:
+    echo.
+    echo The values of -C and -R are interpreted as "name filter"; rules:
     echo - value = whitespace-separated list of items
     echo - items without leading ~ are positive filters, selecting what is included; they are ORed
     echo - items with leading ~ are negative filters, selecting what is excluded; they are ANDed
     echo - all items are evaluated case-insensitively
-    echo.    
+    echo.
     echo Example:
     echo   -C "*closed *count* ~value* ~target*"
     echo   MEANS: All constraints *closed* or *count*, but excluding value* and excluding target*
@@ -135,7 +136,7 @@ if "%schema%"=="?" (
     echo   MEANS: All resources except *.dtd
     echo.
     echo Example:
-    echo   -R "airport-* airpots-* ~*.xml ~*txt" 
+    echo   -R "airport-* airpots-* ~*.xml ~*txt"
     echo   MEANS: All resources airport-* or airports-*, yet excluding *.xml and *.txt
     echo.
     echo.
@@ -153,7 +154,7 @@ rem #    set PARAMS_PARAM=,params=%PARAMS%
 rem #    set PARAMS2=!PARAMS:,=;!
 rem #    set PARAMS_PARAM=,params=!PARAMS2!
 rem #    rem echo "!PARAMS_PARAM!"
-rem #    
+rem #
 rem #)
 rem exit
 
