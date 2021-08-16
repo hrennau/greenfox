81502e84b47a5b03625e20dcdc9e6b7abaef869c
hrennau <hrennau@yahoo.de>
2021-08-16 20:44:57 +0200

    Added testcases for ValueEq and ValueCount constraints.

--- test/bin/execute-greenfox-tests.ps1
+++ test/bin/execute-greenfox-tests.ps1
@@ -1,7 +1,8 @@
 Param(
     $greenfoxDir = "/tt/greenfox",
     $testDir = "$greenfoxDir/test/testcases",
-    $outDir = "$greenfoxDir/test/testcases-output"
+    $outDir = "$greenfoxDir/test/testcases-output",
+    $testcase = $null
 )
 
 $schemas = Get-ChildItem $testDir/*.gfox.xml -Recurse
@@ -9,6 +10,7 @@ $schemas = Get-ChildItem $testDir/*.gfox.xml -Recurse
 foreach ($schema in $schemas) {
     $schemaFS = $schema -replace '\\', '/'
     $fname = $schema.Name
+    if ($testcase) {if (-not($fname -like $testcase)) {continue}}
     $fnameOutput = "output-$fname"
     Write-Host "Execute test: $fname"    
     gfox $schemaFS -r | Out-File "$outDir/$fnameOutput"
