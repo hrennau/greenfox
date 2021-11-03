7d1c22e331c5638ab19a56c9a4101606257029f7
hrennau <hrennau@yahoo.de>
2021-11-03 23:30:06 +0100

    Upgraded example schemas to new vocabulary, for example "navigateFOX".

--- test/bin/execute-example-schemas.ps1
+++ test/bin/execute-example-schemas.ps1
@@ -1,6 +1,6 @@
 Param(
     $rootSchemaDir = "../../../../projects/greenfox/work/greenfox/example-schemas",
-    [Parameter(Mandatory=$true)]$odir,
+    [Parameter(Mandatory=$true)]$odir = '../../../../projects/greenfox/work/validation-output3',
     $pathFilter = $null
 )
 
@@ -23,12 +23,23 @@ $binDir = Join-Path $PSScriptRoot ../../bin | Resolve-Path
 $odir = Resolve-Path $odir
 $gfox = Join-Path $binDir "/gfox.bat"
 foreach ($schema in Get-ChildItem $rootDir/*.gfox.xml -Recurse) {
+    if ($pathFilter) {if (-not($schema -like $pathFilter)) {continue}}
     $fname = $schema.Name
+
+    # Some examples require an explicit domain
+    $domain = 
+        if ($fname -ceq 'cc-links-href.niem.gfox.xml') {"/projects/xsdbase"}
+        elseif ($fname -ceq 'link-targets.recursive.niem.gfox.xml') {"/projects/xsdbase"}
+        elseif ($fname -ceq 'link-targets.recursive.niem.archive.gfox.xml') {"/projects/xsdbase"}
+        elseif ($fname -ceq 'link-targets.recursive.opentravel.gfox.xml') {"/projects/xsdbase"}
+        elseif ($fname -ceq 'link-targets.recursive.opentravel.archive.gfox.xml') {"/projects/xsdbase"}
+        else {""}
+
     $ofile = "output-$fname"
     $opath = "$odir/$ofile"
     $schemaFOX = filename_fox $schema
     Write-Host "Process file: $fname"
-    cmd.exe /c $gfox $schemaFOX -r | Out-File $opath
+    cmd.exe /c $gfox $schemaFOX $domain -r | Out-File $opath
 }
 Write-Host "rootSchemaDir: $rootDir"
 Write-Host "binDir: $binDir"
